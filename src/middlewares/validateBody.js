c81a9645a88b7b1f9026342bf2bcf0ad00b7d66f
Anastasiia Posliednicheko <anastasia29po@gmail.com>
2025-07-17 18:55:11 +0200

    Complete task

---
+++ src/middlewares/validateBody.js
@@ -0,0 +1,15 @@
+import createHttpError from "http-errors";
+
+export const validateBody = (schema) => {
+    return (req, res, next) => {
+        try {
+            const { error } = schema.validate(req.body);
+            if (error) {
+                throw createHttpError(400, error.message);
+            }
+            next();
+        } catch (err) {
+            next(err);
+        }
+    };
+};
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,16 +1,24 @@
 import express from 'express';
-import { getContactsController, getContactByIdController, createContactController, updateContactController, deleteContactController } from '../controllers/contactsController.js';
+import { getContactsController, getContactByIdController, createContactController, updateContactController, deleteContactController, } from '../controllers/contactsController.js';
+
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { isValidId } from '../middlewares/isValidId.js';
+import { contactAddSchema, contactUpdateSchema, } from '../schemas/contactsSchemas.js';
 
 const router = express.Router();
 
 router.get('/', ctrlWrapper(getContactsController));
-router.get('/:contactId', ctrlWrapper(getContactByIdController));
-router.post('/', ctrlWrapper(createContactController));
-router.patch('/:contactId', ctrlWrapper(updateContactController));
-router.delete('/:contactId', ctrlWrapper(deleteContactController));
 
+router.get('/:contactId', isValidId, ctrlWrapper(getContactByIdController));
+
+router.post('/', validateBody(contactAddSchema), ctrlWrapper(createContactController));
+
+router.patch('/:contactId', isValidId, validateBody(contactUpdateSchema), ctrlWrapper(updateContactController));
+
+router.delete('/:contactId', isValidId, ctrlWrapper(deleteContactController));
 
 export default router;
+
 
 
